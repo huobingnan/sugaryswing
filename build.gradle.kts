045e858bc09345de0157817440c51209dd7cc032
huobingnan <huobingnan@126.com>
2021-08-22 12:13:26 +0800

    maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.10"
+    `maven-publish`
 }
 
 group = "sugaryswing"
@@ -15,7 +16,6 @@ repositories {
 dependencies {
 
 }
-
 
 
 tasks.withType<KotlinCompile>() {
